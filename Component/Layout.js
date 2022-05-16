ae2c32d46cf1307f96fb26565b0b3a7b858f2cf1
Gaye Y <96841321+gaiaysl@users.noreply.github.com>
2022-05-16 18:39:13 +0300

    Navbar Update
    Update

--- Component/Layout.js
+++ Component/Layout.js
@@ -7,6 +7,7 @@ export default function Layout({children,categories}){
 <Navbar categories={categories} />
             {children}
 <Footer/>
+
         </div>
     )
 }
--- Component/Navbar.js
+++ Component/Navbar.js
@@ -6,20 +6,25 @@ import styles from '../Component/Navbar.module.scss'
 import Image from "next/image";
 import Link from "next/link";
 import gaia2 from "../Public/images/gaia2.png"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
+
 }
 
+
+
 export default function Navbar({categories}) {
+
     return (
         <Disclosure as="nav" className={styles.nav}>
             {({ open }) => (
                 <>
                     <div className={styles.navContainer}>
                         <div className={styles.navContent}>
+
                             <div className={styles.contentTwo}>
 
                                 {/* Mobile menu button*/}
@@ -42,16 +47,29 @@ export default function Navbar({categories}) {
                                 </div>
                                 <div className={styles.navigation}>
                                     <div className={styles.input}>
-                                        <form>
-                                                <input className={styles.searchBox} type="text" name="name"
-                                                       tabIndex="1"
-                                                       placeholder="Aradığınız ürün, kategori veya markayı yazınız"
-                                                       maxLength="50"/>
-                                            <FontAwesomeIcon  icon="fa-solid fa-magnifying-glass" />
-                                        </form>
+
+                                        <form className={styles.form}>
+                                                    <input className={styles.searchBox} type="text" name="name"
+                                                           tabIndex="1"
+                                                           placeholder="Aradığınız ürün, kategori veya markayı yazınız"
+                                                           maxLength="50"
+                                                    />
+
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className={styles.svg} fill="none"
+                                                                 viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                                                <path strokeLinecap="round" strokeLinejoin="round"
+                                                                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+                                                            </svg>
 
 
+                                                </form>
                                     </div>
+
+
+
+
+
+
 
                                     <div className={styles.navigationTwo}>
                                         {categories &&
@@ -70,32 +88,41 @@ export default function Navbar({categories}) {
                                                     >
                                                         {category}
                                                     </a>
-
-
-
                                         ))}
                                     </div>
                                 </div>
                             </div>
                             <div className={styles.right}>
 
-                                <button
-                                    type="button"
-                                    className={styles.button}
-                                >
-                                    <span className={styles.rightText}>View notifications</span>
-                                    <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
-                                    <BellIcon className={styles.rightButton} aria-hidden="true" />
-                                </button>
-
                                 {/* Profile dropdown */}
                                 <Menu as="div" className="ml-3 relative">
-                                    <div>
-                                        <Menu.Button className={styles.menuButton} >
-                                            <span className={styles.span}>Open user menu</span>
+                                    <div className={styles.icons}>
 
+                                        <Menu.Button className={styles.menuButton} >
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                                 viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                                <path strokeLinecap="round" strokeLinejoin="round"
+                                                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
+                                            </svg><p className="hidden sm:block sm:ml-6">Giriş Yap</p>
                                         </Menu.Button>
+
+                                        <div className={styles.favorite}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                             viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
+                                        </svg><p className="hidden sm:block sm:ml-6">Favorilerim </p>
+                                        </div>
+
+                                        <div className={styles.shopping}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                             viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
+                                        </svg><p className="hidden sm:block sm:ml-6">Sepetim</p>
+                                        </div>
                                     </div>
+
                                     <Transition
                                         as={Fragment}
                                         enter={styles.enter}
@@ -112,7 +139,7 @@ export default function Navbar({categories}) {
                                                         href="#"
                                                         className={classNames(active ? styles.active1 : '', styles.active2)}
                                                     >
-                                                        Your Profile
+                                                        Giriş Yap
                                                     </a>
                                                 )}
                                             </Menu.Item>
@@ -122,17 +149,7 @@ export default function Navbar({categories}) {
                                                         href="#"
                                                         className={classNames(active ? styles.active3 : '', styles.active4 )}
                                                     >
-                                                        Settings
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(active ? styles.active5 : '', styles.active6)}
-                                                    >
-                                                        Sign out
+                                                        Üye Ol
                                                     </a>
                                                 )}
                                             </Menu.Item>
@@ -142,25 +159,29 @@ export default function Navbar({categories}) {
                             </div>
                         </div>
                     </div>
-
                     <Disclosure.Panel className={styles.disc}>
+
                         <div className={styles.panel}>
-                            {categories.map((category) => (
+
+                            {categories &&
+                                categories.length > 0 &&
+                                categories.map((category) => (
                                 <Disclosure.Button
                                     key={category}
                                     as="a"
-                                    href={category}
+                                    href={`/category/${category}`}
                                     className={classNames(
-                                        category ? styles.item1 : styles.item2 ,
+                                        category.current ? styles.item1 : styles.item2 ,
                                         styles.item3
                                     )}
-                                    aria-current={category ? 'page' : undefined}
+                                    aria-current={category.current ? 'page' : undefined}
                                 >
                                     {category}
                                 </Disclosure.Button>
                             ))}
                         </div>
                     </Disclosure.Panel>
+
                 </>
             )}
         </Disclosure>
